362796fa52398ace35529586996dd565b7e0ddca
oladimeji16 <olatohdimeji16@gmail.com>
2020-04-25 21:30:25 +0100

    initial

---
+++ js/script.js
@@ -0,0 +1,10 @@
+(function($){
+    var typed = new Typed('span.txt-rotate', {
+        strings: ["I am a Web Developer", "I am a Mobile Developer", "I am a Digital Marketer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        fadeOut: false,
+        smartBackSpace: true,
+        loop: true
+    });
+})(jQuery);
